5f6f88613c6e3413f9ffeca1257f2e7b8cf3a5b0
Ray-Shakex <bshakex@gmail.com>
2023-09-22 10:01:07 +0200

    aside data-aside + html links

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 const container = document.querySelector('.container');
 const toggle = document.querySelector('.toggle');
 const sidebar = document.querySelector('nav')
-console.log(sidebar);
+const asideLinks = document.querySelectorAll('[data-aside]');
+console.log(asideLinks);
+
+
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle('active');
     toggle.classList.toggle('active');
     container.classList.toggle('active');
+});
+
+asideLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sidebar.classList.remove('active');
+        toggle.classList.toggle('active');
+        container.classList.remove('active');
+    })
 })
+
